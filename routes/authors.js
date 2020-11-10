35e15b29c233b52993364431e990d115b97b709e
alan Dittler <dittler.a@gmail.com>
2020-11-10 20:43:01 -0300

    update

--- routes/authors.js
+++ routes/authors.js
@@ -89,18 +89,18 @@ router.post("/", async (req, res) => {
   }
 });
 
-// router.get("/:id", async (req, res) => {
-//   try {
-//     const author = await Author.findById(req.params.id);
-//     const books = await Book.find({ author: author.id }).limit(6).exec();
-//     res.render("authors/show", {
-//       author: author,
-//       booksByAuthor: books,
-//     });
-//   } catch {
-//     res.redirect("/");
-//   }
-// });
+router.get("/:id", async (req, res) => {
+  try {
+    const author = await Author.findById(req.params.id);
+    const books = await Book.find({ author: author.id }).limit(6).exec();
+    res.render("authors/show", {
+      author: author,
+      booksByAuthor: books,
+    });
+  } catch {
+    res.redirect("/");
+  }
+});
 
 router.get("/:id/edit", async (req, res) => {
   try {
