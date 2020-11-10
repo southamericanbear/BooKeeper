ac43a7eb4a4be46193ceb475586a615e93afa7d4
alan Dittler <dittler.a@gmail.com>
2020-11-10 11:24:49 -0300

    test

--- server.js
+++ server.js
@@ -1,20 +1,10 @@
 if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
+  require("dotenv").load();
 }
 
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
-const mongoose = require("mongoose");
-
-mongoose.connect(process.env.DATABASE_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-const db = mongoose.connection;
-db.on("error", (error) => console.log(error));
-db.once("open", () => console.log("Connected to mongoose"));
 
 const indexRouter = require("./routes/index");
 
@@ -24,6 +14,12 @@ app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
 
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to Mongoose"));
+
 app.use("/", indexRouter);
 
-app.listen(process.env.port || 3000);
+app.listen(process.env.PORT || 3000);
